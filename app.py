142f4ffdd45bae4d1e04b3173aecac4b15c26447
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-22 11:23:03 -0600

    Updated the method names for greater legibility

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+###################################################
+# Python SQL toolkit and Object Relational Mapper #
+###################################################
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect, desc
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -10,20 +17,20 @@ def home():
 
 
 @app.route("/api/v1.0/precipitation")
-def about():
+def precipitation():
     # Todo:Convert the query results to a dictionary using
     #   date as the key and prcp as the value.
     return "Welcome to my 'About' page!"
 
 
 @app.route("/api/v1.0/stations")
-def about():
+def stations():
     # Todo:Return a JSON list of stations from the dataset.
     return "Welcome to my 'About' page!"
 
 
 @app.route("/api/v1.0/tobs")
-def about():
+def tobs():
     # Todo:Query the dates and temperature observations
     #   of the most active station for the last year of data.
     #   Return a JSON list of temperature observations (TOBS) for the previous year.
@@ -31,14 +38,14 @@ def about():
 
 
 @app.route("/api/v1.0/<start>")
-def about():
+def start_temp():
     # Todo:Return a JSON list of the minimum temperature, the average temperature,
     #   and the max temperature for a given start or start-end range.
     return "Welcome to my 'About' page!"
 
 
 @app.route("/api/v1.0/<start>/<end>")
-def about():
+def range_temp():
     # Todo:Return a JSON list of the minimum temperature, the average temperature,
     #   and the max temperature for a given start or start-end range.
     return "Welcome to my 'About' page!"
